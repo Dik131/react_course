25b6386199737cec5d02dfe8047b5c6f25faf62d
Dik131 <d.i.k.93@mail.ru>
2024-03-21 20:32:04 +0300

    lesson 18-152

---
+++ 14-map-array-of-objects/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import persons from './data/persons';
+
+function App() {
+  return (
+    <div className="App">
+      {persons.map((person) => {
+        return <h1>{person.firstName}</h1>;
+      })}
+    </div>
+  );
+}
+
+export default App;
