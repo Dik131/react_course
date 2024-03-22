add7e995dc443876ce961a670e103096d0de9937
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-03-22 14:06:31 +0300

    lesson 18-156

--- 14-map-array-of-objects/src/App.js
+++ 14-map-array-of-objects/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import persons from './data/persons';
+import Person from './components/Person';
 
 function App() {
   return (
     <div className="App">
       {persons.map((person) => {
-        return <h1>{person.firstName}</h1>;
+        //const {id, firstName, lastName, email, img} = person;
+        //return <Person key={id} firstName={firstName} lastName={lastName} email={email} img={img}/>;
+        return <Person key={person.id} {...person} />;
       })}
     </div>
   );
--- 14-map-array-of-objects/src/components/Person.js
+++ 14-map-array-of-objects/src/components/Person.js
@@ -1,1 +1,17 @@
- 
+ import persons from "../data/persons";
+ import './Person.css';
+ const Person = (props) => {
+    console.log(props);
+    const {firstName, lastName, email, img} = props;
+    return (
+      <div className="person">
+        <h1>
+          {firstName} {lastName}
+        </h1>
+        <img src={img} alt='photo'/>
+        <p>{email}</p>
+      </div>
+    );
+  }
+  
+  export default Person
