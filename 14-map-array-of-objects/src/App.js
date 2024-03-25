e2b66e0d2e4827e94f05d93ee2d7d13bcf695c7e
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-03-25 14:14:55 +0300

    lesson 18 - 159

--- 14-map-array-of-objects/src/App.js
+++ 14-map-array-of-objects/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
-import persons from './data/persons';
-import Person from './components/Person';
+//import persons from './data/persons';
+import Persons from './components/Persons';
 
 function App() {
   return (
     <div className="App">
-      {persons.map((person) => {
-        //const {id, firstName, lastName, email, img} = person;
-        //return <Person key={id} firstName={firstName} lastName={lastName} email={email} img={img}/>;
-        return <Person key={person.id} {...person} />;
-      })}
+      <Persons />
     </div>
   );
 }
--- 14-map-array-of-objects/src/components/Person.js
+++ 14-map-array-of-objects/src/components/Person.js
@@ -1,15 +1,18 @@
- import persons from "../data/persons";
+ //import persons from "../data/persons";
  import './Person.css';
  const Person = (props) => {
     console.log(props);
     const {firstName, lastName, email, img} = props;
     return (
+      <div>
+      <div className='spacer'></div>
       <div className="person">
         <h1>
           {firstName} {lastName}
         </h1>
-        <img src={img} alt='photo'/>
+        <img src={img} alt='Profile_image'/>
         <p>{email}</p>
+      </div>
       </div>
     );
   }
