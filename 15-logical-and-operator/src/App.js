0289d01202ed216b80bee69a95532b115c822067
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-03-28 09:43:39 +0300

    lesson 19 hided reset button

--- 15-logical-and-operator/src/App.js
+++ 15-logical-and-operator/src/App.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import './App.css';
-import Counter from './components/Counter';
-import Button from './components/Button';
+import { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+import Button from "./components/Button";
 // import IncrementCount from "./components/IncrementCount";
 
 const App = () => {
@@ -9,23 +9,30 @@ const App = () => {
   const IncrementCount = () => {
     setCount(count + 1);
   };
+  const resetCount = () => {
+    setCount(0);
+  };
 
-  const buttonStyle = { backgroundColor: 'lightblue' };
+  const buttonStyle = { backgroundColor: "lightblue" };
 
   return (
-    <div className="App">
-      <div className="spacer"></div>
+    <div className='App'>
+      <div className='spacer'></div>
       <Counter count={count} />
-      <div className="spacer"></div>
-      <div className="Buttons">
+      <div className='spacer'></div>
+      <div className='Buttons'>
         <Button onClick={IncrementCount} />
         <Button onClick={IncrementCount} />
         <Button onClick={IncrementCount} />
         <Button onClick={IncrementCount} />
       </div>
-      <button style={buttonStyle}>Reset</button>
+      {!!count && (
+        <button style={buttonStyle} onClick={resetCount}>
+          Reset
+        </button>
+      )}
     </div>
   );
 };
-
+//!!count is equal to caunt > 0
 export default App;
