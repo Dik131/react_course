e4c94a6e5df4b2bc2ea7277dc737dea71ea67979
Dik131 <d.i.k.93@mail.ru>
2024-04-02 21:07:09 +0300

    lesson 22

--- 18-controlled-input/src/components/Login.js
+++ 18-controlled-input/src/components/Login.js
@@ -2,32 +2,43 @@ import './Login.css';
 import { useState } from 'react';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+
+  const [data, setData] = useState({ username: '', password: '' });
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const userData = {
-      username: event.target.username.value,
-      password: event.target.password.value,
-    };
-    alert(JSON.stringify(userData));
+    // const userData = {
+    //   username: data.username,
+    //   password: data.password,
+    // };
+    console.log(data);
+    alert(JSON.stringify(data));
   };
+
+  const handleInputChange = (event, name) => {
+    setData({ ...data, [name]: event.target.value });
+  };
+
   return (
     <>
-      <h1>Login</h1>
+      <h1>Login form</h1>
       <form onSubmit={handleSubmit}>
         <label>Username:</label>
         <input
           type="text"
-          name="username"
-          value={username}
+          value={data.username}
+          //onChange={(event) => setData({ ...data, username: event.target.value })}
+          onChange={(event) => handleInputChange(event, 'username')}
           placeholder="Enter your username"
         />
         <label>Password:</label>
         <input
           type="password"
-          name="password"
-          value={password}
+          value={data.password}
+          //onChange={(event) => setData({ ...data, password: event.target.value })}
+          onChange={(event) => handleInputChange(event, 'password')}
           placeholder="********"
         />
         <button type="submit">Login</button>
