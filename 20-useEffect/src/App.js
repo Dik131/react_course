74e54f331be7f6a7a377d9f8e9b05be7cc7a4197
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-04-05 14:00:41 +0300

    lesson 24-187

--- 20-useEffect/src/App.js
+++ 20-useEffect/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const [todo, setTodo] = useState(null);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => setTodo(json));
+  }, []);
+  console.log("App rendered");
+  console.log(todo);
+
+  //this part from https://jsonplaceholder.typicode.com/
+  // fetch("https://jsonplaceholder.typicode.com/todos/1")
+  //   .then((response) => response.json())
+  //   .then((json) => setTodo(json));
+
+  return <div className='App'></div>;
 }
 
 export default App;
