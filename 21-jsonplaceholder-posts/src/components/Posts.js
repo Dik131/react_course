06595b8943db6cd1f581fa2700f2817317230101
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-04-12 14:06:19 +0300

    lesson 25-193

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -1,6 +1,18 @@
 import Post from "./Post";
+import { useState, useEffect } from "react";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => {
+        console.log(posts);
+        setPosts(posts);
+      })
+      .catch((error) => console.log(error.message));
+  }, []);
+
   return (
     <div>
       <Post />
