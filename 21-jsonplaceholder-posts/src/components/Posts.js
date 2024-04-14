6b103c6d97b5900907e89b0d8c105fe90c0b6cba
Dik131 <d.i.k.93@mail.ru>
2024-04-14 18:32:13 +0300

    lesson 25

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -1,30 +1,45 @@
 import Post from "./Post";
 import { useState, useEffect } from "react";
 
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
+
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(API_URL)
       .then((res) => res.json())
       .then((posts) => {
         console.log(posts);
         setPosts(posts);
       })
-      .catch((error) => console.log(error.message));
+      .catch((error) => console.log(error.message))
+      .finally(() => setIsLoading(false));
   }, []);
-
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
   return (
-    <div>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          // id={post.userId}
-          // title={post.title}
-          // body={post.body}
-          {...post}
-        />
-      ))}
-    </div>
+    <>
+      <h1>Posts</h1>
+      <hr />
+      <br />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => (
+          <Post
+            key={post.id}
+            // id={post.userId}
+            // title={post.title}
+            // body={post.body}
+            {...post}
+          />
+        ))
+      )}
+    </>
   );
 };
 
