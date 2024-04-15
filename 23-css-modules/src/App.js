c99108748fec833d1e3f2d064713daa7c27d14e0
Dik131 <d.i.k.93@mail.ru>
2024-04-15 20:05:02 +0300

    first commit lesson 27

--- 23-css-modules/src/App.js
+++ 23-css-modules/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Info from "./components/Info";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <Info />
     </div>
   );
 }
---
+++ 23-css-modules/src/components/Info.js
@@ -0,0 +1,10 @@
+const Info = () => {
+  return (
+    <>
+      <h1>Info</h1>
+      <p>Some info</p>
+      <button>Click me</button>
+    </>
+  );
+};
+export default Info;
