1b750223e53b4cb04934f3979ce31b626b01a3e3
Dik131 <d.i.k.93@mail.ru>
2024-04-16 20:25:35 +0300

    lesson 27

--- 23-css-modules/src/App.js
+++ 23-css-modules/src/App.js
@@ -3,10 +3,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Info />
-      <h1>Hello from App.JS</h1>
-      <button className='btn'>App's button</button>
+      <div className="info">
+        <h1>Hello from App.JS</h1>
+        <h2>Color from info module</h2>
+        <button className="btn">App's button</button>
+      </div>
     </div>
   );
 }
--- 23-css-modules/src/components/Info.js
+++ 23-css-modules/src/components/Info.js
@@ -2,10 +2,10 @@ import "./Info.css";
 import styles from "./Info.module.css";
 const Info = () => {
   return (
-    <div className=''>
+    <div className={styles.info}>
       <h1>Hello from Info.JS</h1>
       <p>Some info</p>
-      <button className='btn'>Click me</button>
+      <button className="btn">Click me</button>
     </div>
   );
 };
