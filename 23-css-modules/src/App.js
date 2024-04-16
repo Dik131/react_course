5970b76f9e2cf49e8bce2727c215e7e35005b26c
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-04-16 13:52:36 +0300

    lesson 27-211

--- 23-css-modules/src/App.js
+++ 23-css-modules/src/App.js
@@ -3,8 +3,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Info />
+      <h1>Hello from App.JS</h1>
+      <button className='btn'>App's button</button>
     </div>
   );
 }
--- 23-css-modules/src/components/Info.js
+++ 23-css-modules/src/components/Info.js
@@ -1,10 +1,12 @@
+import "./Info.css";
+import styles from "./Info.module.css";
 const Info = () => {
   return (
-    <>
-      <h1>Info</h1>
+    <div className=''>
+      <h1>Hello from Info.JS</h1>
       <p>Some info</p>
-      <button>Click me</button>
-    </>
+      <button className='btn'>Click me</button>
+    </div>
   );
 };
 export default Info;
