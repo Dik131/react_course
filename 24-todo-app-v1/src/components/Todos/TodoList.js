c598634a11b965ef50bfdf54c9b02535ad1f6a6d
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-05-28 14:19:35 +0300

    lesson 227

--- 24-todo-app-v1/src/components/Todos/TodoList.js
+++ 24-todo-app-v1/src/components/Todos/TodoList.js
@@ -1,7 +1,14 @@
 import Todo from "./Todo";
 
 const TodoList = ({ todos }) => {
-  return todos.map((todo, index) => <Todo todo={todo} key={index} />);
+  return (
+    <div>
+      {!todos.length && <h2>No Todos</h2>}
+      {todos.map((todo, index) => (
+        <Todo todo={todo} key={index} />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
