9ee57ec8a524db3628cd9fd76170ff8d80039e10
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-05-30 13:57:51 +0300

    lesson 231

---
+++ 25-todo-app-v2/src/components/Todos/TodoForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import styles from "./TodoForm.module.css";
+
+const TodoForm = ({ addTodo }) => {
+  const [text, setText] = useState("");
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    addTodo(text);
+    setText("");
+  };
+  return (
+    <div className={styles.TodoFormWrapper}>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          placeholder='Enter Todo'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type='submit'>Add</button>
+      </form>
+    </div>
+  );
+};
+export default TodoForm;
