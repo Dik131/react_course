43c63a649d9491cc9f06a6be1d0673bbe3357572
Dik131 <d.i.k.93@mail.ru>
2024-06-09 18:56:09 +0300

    Chapter 30

--- 26-react-router/src/App.js
+++ 26-react-router/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="*" element={<h1>404 This page does not exist</h1>} />
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="about" element={<h1>about</h1>} />
+          <Route path="contacts" element={<h1>contacts</h1>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
