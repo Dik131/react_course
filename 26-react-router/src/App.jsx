4610b8d3dc2f757fdab8f9700f2006b27e0a0df2
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-06-10 14:18:54 +0300

    lesson 267

--- 26-react-router/src/App.jsx
+++ 26-react-router/src/App.jsx
@@ -9,13 +9,13 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Routes>
-          <Route path="/" element={<MainLayout />}>
+          <Route path='/' element={<MainLayout />}>
             <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="contacts" element={<Contacts />} />
-            <Route path="*" element={<NotFound />} />
+            <Route path='about' element={<About />} />
+            <Route path='contacts' element={<Contacts />} />
+            <Route path='*' element={<NotFound />} />
           </Route>
         </Routes>
       </div>
