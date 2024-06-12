d6345b7e85082996ffe9d63a48e16acc69e21611
Dik131 <d.i.k.93@mail.ru>
2024-06-12 17:26:46 +0300

    lesson 279

--- 26-react-router/src/components/NotFound.jsx
+++ 26-react-router/src/components/NotFound.jsx
@@ -1,5 +1,5 @@
 function NotFound() {
-  return <h1>Not Found</h1>;
+  return <h1>This page does not exist &#128533;</h1>;
 }
 
 export default NotFound;
--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,13 +1,34 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import courses from "../data/courses";
+// import NotFound from "./NotFound";
+import { useEffect } from "react";
 const SingleCourse = () => {
   const params = useParams();
+  const navigate = useNavigate();
   const course = courses.find((course) => course.slug === params.slug);
+  // a little harder way to show 404 page
+  useEffect(() => {
+    if (!course) {
+      navigate("..", { relative: "path" });
+    }
+  }, [course, navigate]);
+
+  // the simple way to show 404 page
+  // if (!courses.find((course) => course.slug === params.slug)) {
+  //   return (
+  //     <div className="singleCourse">
+  //       <NotFound />
+  //       <Link to=".." relative="path" className="linkBack">
+  //         Choose a course
+  //       </Link>
+  //     </div>
+  //   );
+  // }
   // console.info(params);
   return (
     <div className="singleCourse">
-      <h1>{course.title}</h1>
-      <h2>{course.description}</h2>
+      <h1>{course?.title}</h1>
+      <h2>{course?.description}</h2>
       <Link to=".." relative="path" className="linkBack">
         Back to courses
       </Link>
