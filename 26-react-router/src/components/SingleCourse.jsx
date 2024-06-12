ca758b92cace93288817f356e1094d060e510a3c
Dik131 <d.i.k.93@mail.ru>
2024-06-12 16:42:02 +0300

    lesson 276

--- 26-react-router/src/components/SingleCourse.jsx
+++ 26-react-router/src/components/SingleCourse.jsx
@@ -1,5 +1,18 @@
-function SingleCourse() {
-  return <h1>Single Course info </h1>;
-}
+import { Link, useParams } from "react-router-dom";
+import courses from "../data/courses";
+const SingleCourse = () => {
+  const params = useParams();
+  const course = courses.find((course) => course.slug === params.slug);
+  // console.info(params);
+  return (
+    <div className="singleCourse">
+      <h1>{course.title}</h1>
+      <h2>{course.description}</h2>
+      <Link to=".." relative="path" className="linkBack">
+        Back to courses
+      </Link>
+    </div>
+  );
+};
 
 export default SingleCourse;
