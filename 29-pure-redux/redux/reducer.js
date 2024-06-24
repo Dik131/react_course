473e7475ee62364b2578693209f8e42b0e1db687
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-06-24 14:08:51 +0300

    lesson 313

---
+++ 29-pure-redux/redux/reducer.js
@@ -0,0 +1,14 @@
+const initialState = [];
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_CURRENT_TIME":
+      return [...state, action.payload];
+    case "CLEAR_ALL_CURRENT_TIMES":
+      return [];
+    default:
+      return state;
+  }
+};
+
+export default reducer;
