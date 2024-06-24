8a9bb41bc3e95828cd5c8a162ec7796e14ef264f
Dik131 <d.i.k.93@mail.ru>
2024-06-24 21:38:36 +0300

    lesson 317

---
+++ 29-pure-redux/index.js
@@ -0,0 +1,48 @@
+import store from './redux/store.js';
+import getCurrentTime from './utils/getCurrentTime.js';
+
+//the first realiztion of pure redux
+// const unsubscribe = store.subscribe(() => {
+//   console.log(`store changed ${store.getState()}`);
+// });
+
+// console.log(store.getState());
+
+// store.dispatch({ type: 'ADD_CURRENT_TIME', payload: Date.now() });
+
+// console.log(store.getState());
+
+// unsubscribe();
+// store.dispatch({ type: 'ADD_CURRENT_TIME', payload: '20:18:00' });
+// store.dispatch({ type: 'ADD_CURRENT_TIME', payload: '21:18:00' });
+
+// console.log(store.getState());
+
+// store.dispatch({ type: 'CLEAR_ALL_CURRENT_TIMES' });
+
+// console.log(store.getState());
+
+//the second realization of pure redux
+const addTimeBtn = document.getElementById('addTime');
+addTimeBtn.addEventListener('click', () => {
+  store.dispatch({ type: 'ADD_CURRENT_TIME', payload: getCurrentTime() });
+});
+const timesList = document.getElementById('timesList');
+
+store.subscribe(() => {
+  const times = store.getState();
+  timesList.innerHTML = '';
+  //   while (timesList.firstChild) {
+  //     timesList.removeChild(timesList.firstChild);
+  //   }
+  times.forEach((time) => {
+    const li = document.createElement('li');
+    li.innerText = time;
+    timesList.appendChild(li);
+  });
+});
+
+const clearTimesBtn = document.getElementById('clearTimes');
+clearTimesBtn.addEventListener('click', () => {
+  store.dispatch({ type: 'CLEAR_ALL_CURRENT_TIMES' });
+});
--- 29-pure-redux/redux/store.js
+++ 29-pure-redux/redux/store.js
@@ -1,5 +1,5 @@
-import { configureStore } from "https://cdn.jsdelivr.net/npm/@reduxjs/toolkit@2.2.5/+esm";
-import { reducer } from "./reducer.js";
+import { configureStore } from 'https://cdn.jsdelivr.net/npm/@reduxjs/toolkit@2.2.5/+esm';
+import reducer from './reducer.js';
 
 export default configureStore({
   reducer: reducer,
