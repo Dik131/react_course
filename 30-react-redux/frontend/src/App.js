1ec7e3f0fedef46936890ffa3f0d756528fa461d
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-06-26 13:20:29 +0300

    lesson 322

--- 30-react-redux/frontend/src/App.js
+++ 30-react-redux/frontend/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className='app'>
-      <h1>Book library app</h1>
+      <header className='app-header'>
+        <h1>Book library app</h1>
+      </header>
+      <main className='app-main'></main>
     </div>
   );
 }
