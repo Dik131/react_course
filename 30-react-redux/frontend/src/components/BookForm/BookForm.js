e7175ae77f8fce4732d85f5256de3e7a7c8ab5d4
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-06-26 14:09:22 +0300

    lesson 324

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import "./BookForm.css";
 const BookForm = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  // if it's a lot of input fields, we can use next state to manage them
+  // const [formData, setFormData] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (title && author) {
+      console.log(title, author);
+      setAuthor("");
+      setTitle("");
+    }
+  };
   return (
     <div className='app-block book-form'>
       <h2>Add a new book</h2>
+
+      <form onSubmit={handleSubmit}>
+        <>
+          <label htmlFor='title'>Title:</label>
+          <input
+            type='text'
+            id='title'
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            placeholder='Title'
+          />
+        </>
+        <>
+          <label htmlFor='author'>Author:</label>
+          <input
+            type='text'
+            id='author'
+            value={author}
+            onChange={(event) => setAuthor(event.target.value)}
+            placeholder='Author'
+          />
+        </>
+        <button type='submit'>Add a new book</button>
+      </form>
     </div>
   );
 };
