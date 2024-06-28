064d2b98769ebe76e7db0a4cf5ab49ef699cab74
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-06-28 14:56:28 +0300

    half of lesson 329 but with errors

--- 30-react-redux/frontend/src/components/BookList/BookList.js
+++ 30-react-redux/frontend/src/components/BookList/BookList.js
@@ -1,8 +1,21 @@
+import { useSelector } from "react-redux";
 import "./BookList.css";
 const BookList = () => {
+  const books = useSelector((state) => state.books);
   return (
     <div className='app-block book-list'>
       <h2>Book list</h2>
+      {books.length === 0 ? (
+        <p>No books found</p>
+      ) : (
+        <ul>
+          {books.map((book, i) => (
+            <li key={i}>
+              <div className='book-info'>{book.title} by {book.author}</div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
