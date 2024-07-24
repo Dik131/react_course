239ade1c31ee463df2cefd91ffba44617d067b0e
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-07-24 14:33:10 +0300

    added saving state from Redux to localstorage with Redux-Persist

--- 30-react-redux/frontend/src/index.js
+++ 30-react-redux/frontend/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import ReactDOM from "react-dom/client";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- 30-react-redux/frontend/src/redux/store.js
+++ 30-react-redux/frontend/src/redux/store.js
@@ -1,16 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // import booksReducer from './books/reducer';
-import booksReducer from './slices/booksSlice';
-import filterReducer from './slices/filterSlice';
+import booksReducer from "./slices/booksSlice";
+import filterReducer from "./slices/filterSlice";
 // import filterSlice from './slices/filterSlice';
-import errorReducer from './slices/errorSlice';
+import errorReducer from "./slices/errorSlice";
 
-const store = configureStore({
-  reducer: {
-    books: booksReducer,
-    filter: filterReducer,
-    error: errorReducer,
-  },
+const rootReducer = combineReducers({
+  books: booksReducer,
+  filter: filterReducer,
+  error: errorReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 export default store;
