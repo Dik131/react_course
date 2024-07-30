79c56940a0a554845ba0989f8fb28bcd321cb003
Dik131 <d.i.k.93@mail.ru>
2024-07-30 21:56:37 +0300

    resolved git errors

--- 31-todo-redux/src/App.js
+++ 31-todo-redux/src/App.js
@@ -1,72 +1,15 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  addTodo,
-  toggleTodo,
-  deleteTodo,
-  selectTodos,
-} from "./redux/slices/todoSlice";
-import TodoForm from "./components/Todos/TodoForm";
-import TodoList from "./components/Todos/TodoList";
-import TodosActions from "./components/Todos/TodosActions";
-import "./App.css";
-
-function App() {
-  const todos = useSelector(selectTodos);
-  // const [todos, setTodos] = useState([]);
-  const dispatch = useDispatch();
-  const deleteTodoHandler = (id) => {
-    //setTodos(todos.filter((todo) => todo.id !== id));
-  };
-  const toggleTodoHandler = (id) => {
-    // setTodos(
-    //   todos.map((todo) =>
-    //     todo.id === id ? { ...todo, completed: !todo.completed } : { ...todo }
-    //   )
-    // );
-  };
-  const resetTodosHandler = () => {
-    //setTodos([]);
-  };
-
-  const deleteCompletedTodosHandler = () => {
-    //setTodos(todos.filter((todo) => !todo.completed));
-  };
-
-  const completedTodosCount = todos.filter((todo) => todo.completed).length;
-
-=======
 import TodoForm from './components/Todos/TodoForm';
 import TodoList from './components/Todos/TodoList';
 import TodosActions from './components/Todos/TodosActions';
 import './App.css';
 
 function App() {
->>>>>>> 1b214a1 (added logic for bookForm)
   return (
     <div className='App'>
       <h1>Todo App</h1>
       <TodoForm />
-<<<<<<< HEAD
-<<<<<<< HEAD
-      {!!todos.length && (
-        <TodosActions completedTodosExists={!!completedTodosCount} />
-      )}
 
       <TodoList />
-      {completedTodosCount === 0 ? null : (
-        <p>{`${completedTodosCount} completed ${
-          completedTodosCount === 1 ? "todo" : "todos"
-        }`}</p>
-      )}
-=======
-      <TodosActions />
-=======
-      {/* <TodosActions /> */}
->>>>>>> 47dda6e (the last commit for today)
-      <TodoList />
->>>>>>> 1b214a1 (added logic for bookForm)
     </div>
   );
 }
