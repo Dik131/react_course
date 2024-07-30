7a0edcdca5fbfd92c9d64a23e3b231d8c2119f31
Dik131 <d.i.k.93@mail.ru>
2024-07-30 20:16:28 +0300

    added logic for bookForm

--- 31-todo-redux/src/App.js
+++ 31-todo-redux/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -35,10 +36,19 @@ function App() {
 
   const completedTodosCount = todos.filter((todo) => todo.completed).length;
 
+=======
+import TodoForm from './components/Todos/TodoForm';
+import TodoList from './components/Todos/TodoList';
+import TodosActions from './components/Todos/TodosActions';
+import './App.css';
+
+function App() {
+>>>>>>> 1b214a1 (added logic for bookForm)
   return (
     <div className='App'>
       <h1>Todo App</h1>
       <TodoForm />
+<<<<<<< HEAD
       {!!todos.length && (
         <TodosActions completedTodosExists={!!completedTodosCount} />
       )}
@@ -49,6 +59,10 @@ function App() {
           completedTodosCount === 1 ? "todo" : "todos"
         }`}</p>
       )}
+=======
+      <TodosActions />
+      <TodoList />
+>>>>>>> 1b214a1 (added logic for bookForm)
     </div>
   );
 }
