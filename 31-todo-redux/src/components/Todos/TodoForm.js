db628df97f1ca8c136d432544b3482fe6bc22549
Dik131 <d.i.k.93@mail.ru>
2024-07-30 20:16:28 +0300

    added logic for bookForm

--- 31-todo-redux/src/components/Todos/TodoForm.js
+++ 31-todo-redux/src/components/Todos/TodoForm.js
@@ -1,6 +1,11 @@
 import { useState } from 'react';
+<<<<<<< HEAD
 import { useDispatch } from 'react-redux';
 import { addTodo } from '../../redux/slices/todoSlice';
+=======
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, selectTodos } from '../../redux/slices/todoSlice';
+>>>>>>> 7a0edcd (added logic for bookForm)
 import Button from '../UI/Button';
 import styles from './TodoForm.module.css';
 
