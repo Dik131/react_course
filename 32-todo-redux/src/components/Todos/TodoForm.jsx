5dd808032bd3ae6c6f2caff71d90f58d445c19e6
Dik131 <d.i.k.93@mail.ru>
2024-08-12 20:22:46 +0300

    some estetic chenges

--- 32-todo-redux/src/components/Todos/TodoForm.jsx
+++ 32-todo-redux/src/components/Todos/TodoForm.jsx
@@ -1,19 +1,18 @@
-import { useState } from 'react';
 import Button from '../UI/Button';
 import styles from './TodoForm.module.css';
 
 const TodoForm = ({ text, handleTextChange, handleSubmit }) => {
   return (
-    <div className={styles.TodoFormWrapper}>
+    <form className={styles.TodoFormWrapper}>
       <input
         placeholder='Enter Todo'
         value={text}
         onChange={(e) => handleTextChange(e.target.value)}
       />
-      <button onClick={handleSubmit} disabled={!text.trim()}>
+      <Button onClick={handleSubmit} disabled={!text.trim()}>
         Add Todo
-      </button>
-    </div>
+      </Button>
+    </form>
   );
 };
 export default TodoForm;
