6a7d670450c9a55e0a2826bf8855c2e85ac9bcb7
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-08-29 14:16:06 +0300

    styled theme switcher

--- 34-my-todo-app/src/App.jsx
+++ 34-my-todo-app/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import ThemeSwitch from './components/ThemeSwitch'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <h1>Weeky</h1>
       <ThemeSwitch />
-      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+      
     </>
   )
 }
