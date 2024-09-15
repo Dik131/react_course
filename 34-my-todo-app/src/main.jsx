0f9d829d7799a178eb5c17fae21bf94ebb19e30c
Dik131 <d.i.k.93@mail.ru>
2024-09-15 19:18:46 +0300

    offline funcionaality

--- 34-my-todo-app/src/main.jsx
+++ 34-my-todo-app/src/main.jsx
@@ -1,12 +1,27 @@
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log(
+          'Service Worker registered with scope:',
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
